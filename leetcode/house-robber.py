1d91ef6a2b21795fc52e499123946d249df91370
Hyeokwoo Kwon <pllapallpal@gmail.com>
2022-01-21 01:08:08 +0900

    Update leetcode/house-robber

--- leetcode/house-robber.py
+++ leetcode/house-robber.py
@@ -7,13 +7,14 @@ class Solution:
         if len_nums == 1:
             return nums[0]
         maxs = [0 for i in range(len_nums)]
-        maxs[0], maxs[1] = nums[0], nums[1]
+        maxs[0], maxs[1] = nums[0], max(nums[0], nums[1])
         for i in range(2, len_nums):
-            maxs[i] = max(maxs[i - 1], maxs[i - 2] + nums[i], maxs[i - 3] + nums[i])
-        return max(maxs[-1], maxs[-2])
+            maxs[i] = max(maxs[i - 1], maxs[i - 2] + nums[i])
+        return maxs[-1]
 
 
 if __name__ == '__main__':
     print(Solution().rob([1, 2, 3, 1]))
     print(Solution().rob([2, 7, 9, 3, 1]))
     print(Solution().rob([2, 1, 1, 2]))
+    print(Solution().rob([1, 2, 3, 1]))
