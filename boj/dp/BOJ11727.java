71e8f345d1f6607a25db1540d47220804334b2e0
pllap <pllapallpal@gmail.com>
2022-03-06 18:43:49 +0900

    Solve boj/dp/11727. 2xn 타일링 2

---
+++ boj/dp/BOJ11727.java
@@ -0,0 +1,24 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class BOJ11727 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(bufferedReader.readLine());
+
+        if (n == 1) {
+            System.out.println("1");
+            return;
+        }
+
+        int[] mem = new int[n + 1];
+        mem[1] = 1;
+        mem[2] = 3;
+        for (int i = 3; i < n + 1; ++i) {
+            mem[i] = (mem[i - 2] * 2 + mem[i - 1]) % 10007;
+        }
+
+        System.out.println(mem[n]);
+    }
+}
