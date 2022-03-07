7ca805469812cca6b57cbaeb5b0fe295faae05b1
pllap <pllapallpal@gmail.com>
2022-03-07 10:13:21 +0900

    Re-solve leetcode/21. Merge Two Sorted Lists with Java

---
+++ leetcode/LC21.java
@@ -0,0 +1,71 @@
+package com.pllap.leetcode;
+
+public class LC21 {
+
+    public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
+
+        if (list1 == null) {
+            return list2;
+        } else if (list2 == null) {
+            return list1;
+        }
+
+        ListNode head = new ListNode();
+        ListNode node = head;
+        while (list1 != null && list2 != null) {
+            if (list1.val < list2.val) {
+                node.next = new ListNode(list1.val);
+                list1 = list1.next;
+            } else {
+                node.next = new ListNode(list2.val);
+                list2 = list2.next;
+            }
+            node = node.next;
+        }
+
+        if (list1 != null) {
+            node.next = list1;
+        } else {
+            node.next = list2;
+        }
+
+        return head.next;
+    }
+
+    public static void main(String[] args) {
+        ListNode list1, list2, result;
+
+        list1 = new ListNode(1, new ListNode(2, new ListNode(4)));
+        list2 = new ListNode(1, new ListNode(3, new ListNode(4)));
+        result = new LC21().mergeTwoLists(list1, list2);
+        while (null != result) {
+            System.out.println(result.val);
+            result = result.next;
+        }
+
+        list1 = null;
+        list2 = null;
+        result = new LC21().mergeTwoLists(list1, list2);
+        while (null != result) {
+            System.out.println(result.val);
+            result = result.next;
+        }
+    }
+
+    static class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode() {
+        }
+
+        ListNode(int val) {
+            this.val = val;
+        }
+
+        ListNode(int val, ListNode next) {
+            this.val = val;
+            this.next = next;
+        }
+    }
+}
